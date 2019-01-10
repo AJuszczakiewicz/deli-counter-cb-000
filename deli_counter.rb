e76c41a4470d0cf157324ffcbaef689dc54835f4
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 15:45:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
     puts "The line is currently empty."
   else
     phrase = "The line is currently:"
-    array.each_with_index {|name, index| phrase << " #{index}. #{name}"}
+    array.each_with_index {|name, index| phrase << " #{index+1}. #{name}"}
     puts phrase
   end
 end
