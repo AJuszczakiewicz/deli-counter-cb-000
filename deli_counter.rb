afd12e514c9e6795da02562ee48cf0b4ff3e6371
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 15:43:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    phrase = "The line is currently: "
+    phrase = "The line is currently:"
     array.each_with_index {|name, index| phrase << " #{index}. #{name}"}
     binding.pry
     phrase
