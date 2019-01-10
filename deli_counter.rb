bc9bca8d60d42a7b991bcd063a674ac674964dc4
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-10 15:49:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array.size} in line."
+  puts "Welcome, #{name}. You are number #{array.size+1} in line."
 end
 
 def now_serving
